55fb64880ff9e7cc4c507e0e308399403e1cfbc0
AllenCodess <Aph296@protonmail.com>
2026-07-23 10:43:43 -0400

    had to remove the callback function because synchronous functions dont return cb functions

--- app.js
+++ app.js
@@ -3,25 +3,9 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-// // defines the route and the handler that runs when that route is hit
-// app.get('/', (req, res) => {
-//   res.status(404).send('Hello from Express');
-// });
-
-// // declares which port to listen on
-// app.listen(port, () => {
-//   console.log(`app running on port ${port}`);
-// });
-
 const tours = fs.readFileSync(
   `${__dirname}/dev-data/data/tours-simple.json`,
   'utf-8',
-  (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    res.send(data);
-  },
 );
 
 app.get('/api/v1/tours', (req, res) => {
