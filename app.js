927ef0ca9a75d808d4fdb2a58f5996ecff0f6cac
AllenCodess <Aph296@protonmail.com>
2026-07-23 10:24:56 -0400

    created a route that sends back the tours variable. The tours variable will read a file and send that file back in the response if successful. If an error it will log the error.

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-// defines the route and the handler that runs when that route is hit
-app.get('/', (req, res) => {
-  res.status(404).send('Hello from Express');
+// // defines the route and the handler that runs when that route is hit
+// app.get('/', (req, res) => {
+//   res.status(404).send('Hello from Express');
+// });
+
+// // declares which port to listen on
+// app.listen(port, () => {
+//   console.log(`app running on port ${port}`);
+// });
+
+const tours = fs.readFileSync(
+  `${__dirname}/dev-data/data/tours-simple.json`,
+  'utf-8',
+  (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(data);
+  },
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.send(tours);
 });
 
-// declares which port to listen on
 app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+  console.log(`app listening on port ${port}`);
 });
