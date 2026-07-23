fa8bae4b333f6e481315e6a9f578e2a9d9e1ccc3
AllenCodess <Aph296@protonmail.com>
2026-07-23 16:57:53 -0400

    can now add tours to the api, configured the post api

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-const tours = fs.readFileSync(
-  `${__dirname}/dev-data/data/tours-simple.json`,
-  'utf-8',
+// this is how I can use middleware. middleware sits inbetween the request and the response
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'),
 );
 
 app.get('/api/v1/tours', (req, res) => {
   res.send(tours);
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    'utf-8',
+    (err) => {
+      res.send(newTour);
+    },
+  );
 });
 
 app.listen(port, () => {
