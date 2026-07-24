cede24fba49e3c70e9b4d95ccb94d1a6fc914d6c
AllenCodess <Aph296@protonmail.com>
2026-07-24 08:46:17 -0400

    commented the original refactor

--- app.js
+++ app.js
@@ -55,11 +55,11 @@ const createTour = (req, res) => {
   );
 };
 
-app.get('/api/v1/tours', getAllTours);
-app.post('/api/v1/tours', createTour);
-app.get('/api/v1/tours/:id', getSingleTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getSingleTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
