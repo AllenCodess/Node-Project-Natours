5c0fb8a3453809e4afebb54989570039c8a9aad3
AllenCodess <Aph296@protonmail.com>
2026-07-29 13:33:59 -0400

    added mongoose and connected the remote DB to the app

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const { app, port } = require('./app');
+const mongoose = require('mongoose');
 
-// 4) SERVER
+const DB = process.env.DATABASE;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log('Successfully connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error.message);
+  }
+};
+
+connectDB();
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
