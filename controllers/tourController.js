759b15724393f59107250707955ce81210bb8183
AllenCodess <Aph296@protonmail.com>
2026-07-30 10:00:03 -0400

    can query all tours via database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,18 @@
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  res.send('getting all tours');
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      data: tours,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
 
 exports.getSingleTour = (req, res) => {
