bd6d5235e623939c8ea85a75c3f9bbc719f8f2b2
AllenCodess <Aph296@protonmail.com>
2026-07-30 21:19:12 -0400

    updated the tour Model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -51,7 +51,7 @@ exports.updateTour = async (req, res) => {
 exports.deleteTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndDelete(req.params.id);
-    res.status(200).json({
+    res.status(204).json({
       status: 'success',
     });
     console.log('the tour has been deleted');
--- models/tourModel.js
+++ models/tourModel.js
@@ -5,9 +5,37 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: { type: Number, default: 4.5 },
+  duration: {
+    type: Number,
+    required: [true, 'A group must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a maxGroupSize'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Agroup must have a difficulty'],
+  },
+  ratingAverage: { type: Number, default: 4.5 },
+  ratingQuantity: { type: Number, deafult: 0 },
   price: { type: Number, required: [true, 'A tour must have a price'] },
+  priceDiscount: { type: Number },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour  must have a summary'],
+  }, // removes all whitespace in string
+  description: { type: String, trim: true },
+  imageCover: {
+    type: String,
+    required: [true, ' A tour must have a imageCover'],
+  },
+  images: [String],
+  createdAt: { type: Date, default: Date.now() },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
