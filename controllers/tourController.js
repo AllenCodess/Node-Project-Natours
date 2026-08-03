a59ee668f8f1091e04285ab6f15ad1e40ccee8dd
AllenCodess <Aph296@protonmail.com>
2026-08-03 19:50:18 -0400

    added pagination logic

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -26,6 +26,13 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
     // Execute Query
     let tours = await query; // executes query and waits for database results
 
