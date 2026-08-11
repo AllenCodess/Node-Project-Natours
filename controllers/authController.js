ea859b10c5656aac036708664dc81b5e3906b23b
AllenCodess <Aph296@protonmail.com>
2026-08-10 20:38:54 -0400

    started the login process

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const jwt = require('jsonwebtoken');
+const AppError = require('./../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -22,3 +23,18 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body; // this is how the user sends login credentials
+
+  // 1) Check if password and email exist in the post request
+  if (!email || !password) {
+    next(new AppError('Please provide email and password!', 400));
+  }
+
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
